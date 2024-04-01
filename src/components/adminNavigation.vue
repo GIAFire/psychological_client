a10e1bcd4fef3c90001d437092a29198eae440ca
xiaoxinPro14 <723159401@qq.com>
2024-04-01 20:07:56 +0800

    新增管理员导航栏组件

---
+++ src/components/adminNavigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="admin-navigation">
+    <h1>Admin Navigation</h1>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+    .admin-navigation {
+        width: 100%;
+        height: 100%;
+        background-color: #fff;
+    }
+</style>
